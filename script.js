fa83e5da43e6c75f86f0a2dce3b2e17bdded875e
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-10-28 14:54:16 +0100

    Pridavam priklad s DOM

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const headerElement = document.querySelector('.header');
 const puvodniObsah = headerElement.textContent;
 headerElement.textContent = 'Dobré ráno';
 
-// console.log(headerElement)
+// console.log(headerElement.textContent)
+// document.body.innerHTML += headerElement.textContent
 
-// document.body.innerHTML += headerElement
+headerElement.style.color = 'white';
+headerElement.style.marginBottom = '2rem'
+
+//headerElement.classList.add('new-header');
+
+const natureElement = document.querySelector('.nature');
+natureElement.src = 'images/poust.jpg';
+natureElement.alt = 'Poust';
+natureElement.style.width = '500px'
+
+const cardElement = document.querySelector('#card2');
+cardElement.style.color = 'red'
