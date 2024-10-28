21b738a637852fede9ab35ffba486c651fd97ebf
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-10-28 13:25:37 +0100

    Pridavam upravene uvodni soubory

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ const headerElement = document.querySelector('.header');
 const puvodniObsah = headerElement.textContent;
 headerElement.textContent = 'Dobré ráno';
 
-console.log(headerElement)
+// console.log(headerElement)
 
-// const jmeno = "kkkk"
-// console.log(jmeno)
-
-// const title = "Pan prstenu"
-// console.log(title.length)
-// console.log(title.toUpperCase())
-
-// const headerElement = document.querySelector('.header');
-// headerElement.style.color = 'white';
-// headerElement.style.marginBottom = '2rem';
+// document.body.innerHTML += headerElement
